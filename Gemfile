80bb324096c272a830b54f0da052e715f22fd42b
Brian Choy <bycEEE@gmail.com>
2015-04-07 09:36:01 -0400

    Add rspotify gem and spotify credentials

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'figaro'
+gem 'rspotify'
 
 gem 'omniauth-facebook', '~> 2.0.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
 end
