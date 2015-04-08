b988ca3841f50ff5fe936d82be2b0cf36ed7ae12
Brian Choy <bycEEE@gmail.com>
2015-04-08 15:45:09 -0400

    Fix login creating multiple accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ActiveRecord::Base
   has_many :artists, through: :artist_users
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.info.id).first_or_create do |user|
     user.provider = auth.provider
     user.uid = auth.info.id
     user.name = auth.info.display_name
     user.email = auth.info.email
     user.image_url = auth.info.images.first.url
     user.profile_url = auth.info.external_urls[:spotify]
-    user.oauth_token = auth.credentials[:token]
     user.save
     end
   end
