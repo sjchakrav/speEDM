9bdb3182326ea438359afec51ef8c73211652e9f
Brian Choy <bycEEE@gmail.com>
2015-04-09 14:56:15 -0400

    Show all top tracks from artists user follows

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @artists = @user.artists.all
-    # @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'])
   end
-  # def search
-  #   @artist_searched = RSpotify::Artist.search(params[:search][:name]).first 
-  #   redirect_to artist_path(@artist)
-
-  #  # @search =  Search.new
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
-  resources :artists #, only: [:index, :new, :create, :show]
-  resources :users
-  resources :artist_users
+  resources :artists, only: [:new, :create, :show, :destroy]
+  resources :users, only: [:new, :create, :show, :destroy]
+  resources :artist_users, only: [:new, :create, :show, :destroy]
   post "/search", to: 'search#search'
 
   # Authentication
-  # get '/auth/spotify/callback', to: 'users#show'
   get '/auth/spotify/callback', to: 'sessions#create'
   get 'signout', to: 'sessions#destroy'
   match 'auth/failure', to: redirect('/'), via: :get
