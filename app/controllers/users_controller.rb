a1362dc4add66abf993719c75b1187acee6bf10d
Brian Choy <bycEEE@gmail.com>
2015-04-09 22:00:55 -0400

    Move track display logic from view to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @artists = @user.artists.all
-    @client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,39 @@ class User < ActiveRecord::Base
   has_many :artist_users
   has_many :artists, through: :artist_users
 
+  def spotify_tracks
+    spotify_hash = {}
+    i = 1
+    artists.each do |artist|
+      tracks = RSpotify::Artist.search(artist.name).first.top_tracks("US")
+      tracks.each do |track|
+        spotify_hash[i.to_s] ||= {}
+        spotify_hash[i.to_s]["artist_name"] = artist.name
+        spotify_hash[i.to_s]["track_name"] = track.name
+        spotify_hash[i.to_s]["album_name"] = track.album.name
+        spotify_hash[i.to_s]["image_url"] = track.album.images.first["url"]
+        i += 1
+      end
+    end
+    spotify_hash
+  end
+
+  def soundcloud_tracks
+    soundcloud_hash = {}
+    i = 1
+    artists.each do |artist|
+      client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_ID'])
+      tracks = client.get('/tracks', :q => "#{artist.name}", :licence => 'cc-by-sa')
+      tracks.each.with_index(1) do |track, i|
+        soundcloud_hash[i.to_s] ||= {}
+        soundcloud_hash[i.to_s]["artist_name"] = artist.name
+        soundcloud_hash[i.to_s]["track_name"] = track.title
+        soundcloud_hash[i.to_s]["image_url"] = track.artwork_url
+      end
+    end
+    soundcloud_hash
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.info.id).first_or_create do |user|
     user.provider = auth.provider
