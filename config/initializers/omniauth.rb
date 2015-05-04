a32c96b475403360d501db669dc762c7076a725f
sjchakrav <sjchakrav@gmail.com>
2015-05-04 13:00:35 -0400

    update home and user views

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'], scope: 'user-read-email user-follow-read'
+  provider :spotify, ENV['SPOTIFY_KEY'], ENV['SPOTIFY_SECRET'], scope: 'user-read-email user-follow-read'
 end
 
 
