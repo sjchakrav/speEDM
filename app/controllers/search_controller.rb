c9f6c0e22ba751440c1ef01dfd16da438491a5aa
Christopher Lee <christopher.lee681@gmail.com>
2015-05-10 12:26:05 -0400

    Refactor search action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,30 @@
 class SearchController < ApplicationController
   def search
-    @artist = Artist.find_by(:slug => search_params["name"].downcase.gsub(/[^a-z1-9$]+/, '-').chomp('-'))
+    @artist = Artist.find_by(slug: normalized_artist_name)
+
     unless @artist
       found = RSpotify::Artist.search(search_params["name"]).first
-      if found.images.empty?
-        @artist = Artist.create_artist_from_spotify(found.name, nil, found.external_urls["spotify"])
-      else
-        @artist = Artist.create_artist_from_spotify(found.name, found.images[0]["url"], found.external_urls["spotify"])
-      end
+      persist_artist(found)
       Track.create_tracks(@artist)
     end
+
     redirect_to artist_path(@artist)
   end
 
   private
+
+  def normalized_artist_name
+    search_params["name"].downcase.gsub(/[^a-z1-9$]+/, '-').chomp('-')
+  end
+
+  def persist_artist(artist)
+    image_url = artist.images.empty? ? nil : artist.images[0]['url']
+
+    @artist = Artist.create_artist_from_spotify(
+                artist.name,
+                image_url,
+                artist.external_urls["spotify"])
+  end
 
   def search_params
     params.require(:search).permit(:name)
